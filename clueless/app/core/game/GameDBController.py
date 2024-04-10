26e83376a49453663803ec767960f2e7b50b7cdc
deleomike <michaeldeleo31@yahoo.com>
2024-04-10 19:40:53 -0400

    function comments

--- clueless/app/core/game/GameDBController.py
+++ clueless/app/core/game/GameDBController.py
@@ -16,6 +16,9 @@ from clueless.app.db.crud.character import CharacterCRUD, CharacterCreate, Chara
 
 
 class GameDBController:
+    """
+    The main interface for
+    """
 
     def __init__(self, game_id: UUID, session):
         self.id = game_id
@@ -26,21 +29,45 @@ class GameDBController:
         self.character_crud = CharacterCRUD(session=self.session)
 
     @property
-    def full_state(self) -> GameRead:
+    def full_state(self) -> GameReadWithLinks:
+        """
+        Full Game State
+
+        :return:
+        """
         return self.game_crud.get(self.id)
 
     def get_adjacent_character_locations(self, character_id: UUID) -> List[LocationRead]:
+        """
+        Get the rooms adjacent to the character
+
+        :param character_id:
+        :return:
+        """
         character = self.character_crud.get_with_link(character_id)
 
         return self.get_adjacent_locations(location_id=character.location_id)
 
 
     def get_adjacent_locations(self, location_id: UUID) -> List[LocationRead]:
+        """
+        Get the adjacent rooms to a given location.
+
+        :param location_id:
+        :return:
+        """
         location = self.location_crud.get(location_id)
 
         return location.connected_locations
 
     def is_valid_location_move(self, character_id: UUID, location_id: UUID) -> bool:
+        """
+        Checks whether a move is invalid (no connection between locations)
+
+        :param character_id:
+        :param location_id:
+        :return:
+        """
         character = self.character_crud.get_with_link(character_id)
 
         if character.location_id == location_id:
@@ -54,9 +81,42 @@ class GameDBController:
         return False
 
     def get_character_info(self, user_id: UUID) -> CharacterReadLinks:
+        """
+        Gets the character info by the user's ID
+
+        :param user_id:
+        :return:
+        """
         return self.character_crud.get_by_user_id(user_id=str(user_id))
 
+    def move_player_location_name(self, character_id: UUID, location_name: str, validate: bool = False) -> CharacterReadLinks:
+        """
+        Move a character to a new room by the name of the room
+
+        :param character_id:
+        :param location_name:
+        :param validate:
+        :return:
+        """
+        game = self.full_state
+
+        for location in game.locations:
+            if location.name == location_name:
+                return self.move_player(
+                    character_id=character_id,
+                    location_id=location.id,
+                    validate=validate
+                )
+
     def move_player(self, character_id: UUID, location_id: UUID, validate: bool = False) -> CharacterReadLinks:
+        """
+        Move a character to a new room by the location's id
+
+        :param character_id:
+        :param location_id:
+        :param validate:
+        :return:
+        """
         if validate:
             valid = self.is_valid_location_move(character_id=character_id, location_id=location_id)
 
